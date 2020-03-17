f379cee07cd7fb00d4f3b1a81db35f1e0393a45f
leqi200 <693472577@qq.com>
2020-03-17 11:52:21 +0800

    ..

--- common-client/src/main/java/com/example/test/main/Run.java
+++ common-client/src/main/java/com/example/test/main/Run.java
@@ -3,10 +3,16 @@ package com.example.test.main;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 /**
  * Description
@@ -14,19 +20,35 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Qi
  * @date 2020年3月10日 下午3:52:58
 **/
-@EnableEurekaClient
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
 @RestController
+@EnableHystrix
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class Run {
-	public static void main(String[] args) {
-		SpringApplication.run(Run.class, args);
-	}
-	
+
+    /**
+     * 访问地址 http://localhost:8762/actuator/hystrix.stream
+     * @param args
+     */
+
+    public static void main(String[] args) {
+        SpringApplication.run( Run.class, args );
+    }
+
     @Value("${server.port}")
     String port;
 
     @RequestMapping("/hi")
+    @HystrixCommand(fallbackMethod = "hiError")
     public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
         return "hi " + name + " ,i am from port:" + port;
     }
+
+    public String hiError(String name) {
+        return "hi,"+name+",sorry,error!";
+    }
+
 }
