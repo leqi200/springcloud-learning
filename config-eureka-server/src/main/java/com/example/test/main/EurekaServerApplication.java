39dd9282107ac4afce1968946bf7b9589a36b099
leqi200 <693472577@qq.com>
2020-03-17 09:14:16 +0800

    .

---
+++ config-eureka-server/src/main/java/com/example/test/main/EurekaServerApplication.java
@@ -0,0 +1,20 @@
+package com.example.test.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Description
+ *
+ * @author Qi
+ * @date 2020年3月16日 下午6:20:54
+**/
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
